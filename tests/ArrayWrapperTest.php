60d0a32481e4435059384a6635d932e9de594bc2
Michael Jennings <michael.jennings91@gmail.com>
2015-09-30 16:49:35 +0100

    Add array wrapper test

---
+++ tests/ArrayWrapperTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests;
+
+use Michaeljennings\Carpenter\Wrappers\ArrayWrapper;
+
+class ArrayWrapperTest extends TestCase
+{
+    public function testDataCanBeAccessedDynamically()
+    {
+        $wrapper = $this->makeWrapper();
+
+        $this->assertEquals('Test', $wrapper->test);
+        $this->assertEquals('Test 1', $wrapper->foo);
+    }
+
+    protected function makeWrapper()
+    {
+        return new ArrayWrapper($this->getData()[0]);
+    }
+}
