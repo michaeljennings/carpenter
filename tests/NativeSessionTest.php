3efc677476f6fbb61ca225fa40c694d6cf53fa8f
Michael Jennings <michael.jennings91@gmail.com>
2015-09-30 16:19:00 +0100

    Add native session test

---
+++ tests/NativeSessionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests;
+
+use Michaeljennings\Carpenter\Session\NativeDriver;
+
+class NativeSessionTest extends TestCase
+{
+    public function testImplementsSessionContract()
+    {
+        $session = $this->makeNativeSession();
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Session', $session);
+    }
+
+    public function testDataCanBeStoredInTheSession()
+    {
+        $session = $this->makeNativeSession();
+
+        $this->assertEquals('bar', $session->put('foo', 'bar'));
+    }
+
+    public function testDataCanBeRetrievedFromTheSession()
+    {
+        $session = $this->makeNativeSession();
+
+        $this->assertEquals('bar', $session->put('foo', 'bar'));
+        $this->assertEquals('bar', $session->get('foo'));
+    }
+
+    public function testDataCanBeRemovedFromTheSession()
+    {
+        $session = $this->makeNativeSession();
+
+        $session->put('foo', 'bar');
+
+        $this->assertEquals('bar', $session->get('foo'));
+
+        $session->forget('foo');
+
+        $this->assertFalse($session->get('foo'));
+    }
+
+    public function testHasChecksIfDataInSession()
+    {
+        $session = $this->makeNativeSession();
+
+        $session->put('foo', 'bar');
+
+        $this->assertTrue($session->has('foo'));
+
+        $session->forget('foo');
+
+        $this->assertFalse($session->has('foo'));
+    }
+
+    public function testFlashStoresItemInSession()
+    {
+        $session = $this->makeNativeSession();
+
+        $this->assertNull($session->flash('foo', 'bar'));
+        $this->assertEquals('bar', $session->get('foo'));
+    }
+
+    protected function makeNativeSession()
+    {
+        return new NativeDriver($this->getConfig());
+    }
+}
