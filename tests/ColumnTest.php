efece642d0ed7940c62726f4de18afe47dfd5a15
Michael Jennings <michael.jennings91@gmail.com>
2015-09-30 14:05:21 +0100

    Add column test

---
+++ tests/ColumnTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests;
+
+class ColumnTest extends TestCase
+{
+    public function testLabelCanBeSetAndRetrieved()
+    {
+        $table = $this->makeTable();
+
+        $column = $table->column('test')->setLabel('Foo');
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Column', $column);
+        $this->assertContains('Foo', $column->getLabel());
+    }
+
+    public function testHrefIsCreatedAutomatically()
+    {
+        $table = $this->makeTable();
+
+        $column = $table->column('test')->setLabel('Foo');
+
+        $this->assertContains('test', $column->getHref());
+    }
+
+    public function testCustomSortCanBeSetForColumns()
+    {
+        $table = $this->makeTable();
+
+        $column = $table->column('test')->sort(function($q) {
+            $q->orderBy('foo');
+        });
+
+        $this->assertTrue($column->hasSort());
+        $this->assertInstanceOf('Closure', $column->getSort());
+    }
+
+    public function testColumnCanBeMadeSortable()
+    {
+        $table = $this->makeTable();
+
+        $column = $table->column('test')->unsortable();
+
+        $this->assertFalse($column->isSortable());
+
+        $column->sortable();
+
+        $this->assertTrue($column->isSortable());
+    }
+}
