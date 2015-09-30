8ca747f9cd6944201938bbff28ac7ee20beffbe7
Michael Jennings <michael.jennings91@gmail.com>
2015-09-30 17:58:14 +0100

    Add row test

---
+++ tests/RowTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests;
+
+use Michaeljennings\Carpenter\Components\Action;
+use Michaeljennings\Carpenter\Components\Cell;
+use Michaeljennings\Carpenter\Components\Column;
+use Michaeljennings\Carpenter\Components\Row;
+use Michaeljennings\Carpenter\Session\SessionManager;
+
+class RowTest extends TestCase
+{
+    public function testRowImplementsContract()
+    {
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Row', $this->makeRow());
+    }
+
+    public function testCellCanBeAddedToRow()
+    {
+        $row = $this->makeRow();
+
+        $row->addCell('foo', $this->makeCell('foo', 'bar', ['foo' => 'bar']));
+
+        $this->assertTrue($row->hasCells());
+        $this->assertCount(1, $row->getCells());
+    }
+
+    public function testActionCanBeAddedToRow()
+    {
+        $row = $this->makeRow();
+
+        $row->addAction($this->makeAction());
+
+        $this->assertTrue($row->hasActions());
+        $this->assertCount(1, $row->getActions());
+    }
+
+    public function testIdCanBeSetOnRow()
+    {
+        $row = $this->makeRow();
+
+        $row->setId('1');
+
+        $this->assertEquals('1', $row->getId());
+    }
+
+    public function testCellAliasesWork()
+    {
+        $row = $this->makeRow();
+
+        $row->cell('foo', $this->makeCell('foo', 'bar', ['foo' => 'bar']));
+
+        $this->assertTrue($row->hasCells());
+        $this->assertCount(1, $row->cells());
+    }
+
+    public function testActionAliasesWork()
+    {
+        $row = $this->makeRow();
+
+        $row->action($this->makeAction());
+
+        $this->assertTrue($row->hasActions());
+        $this->assertCount(1, $row->actions());
+    }
+
+    protected function makeRow()
+    {
+        return new Row();
+    }
+
+    protected function makeCell($key, $value, $data)
+    {
+        $config = $this->getConfig();
+
+        return new Cell($value, $data, new Column($key, 'test', new SessionManager($config), $config));
+    }
+
+    protected function makeAction()
+    {
+        return new Action('create');
+    }
+}
