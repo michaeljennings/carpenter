5be342bd56695595746868eb5dbfd655ffca8dc6
Michael Jennings <michael.jennings91@gmail.com>
2015-10-01 17:08:24 +0100

    Add codeigniter session test

---
+++ tests/Session/CodeigniterSessionTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests\Session {
+
+    use Michaeljennings\Carpenter\Session\CodeigniterDriver;
+    use Michaeljennings\Carpenter\Tests\TestCase;
+
+    class CodeigniterSessionTest extends TestCase
+    {
+        public function testImplementsSessionContract()
+        {
+            $session = $this->makeSession();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Session', $session);
+        }
+
+        public function testDataCanBeStoredInTheSession()
+        {
+            $session = $this->makeSession();
+
+            $this->assertTrue($session->put('foo', 'bar'));
+        }
+
+        public function testDataCanBeRetrievedFromTheSession()
+        {
+            $session = $this->makeSession();
+
+            $this->assertTrue($session->put('foo', 'bar'));
+            $this->assertEquals('bar', $session->get('foo'));
+        }
+
+        public function testDataCanBeRemovedFromTheSession()
+        {
+            $session = $this->makeSession();
+
+            $session->put('foo', 'bar');
+
+            $this->assertEquals('bar', $session->get('foo'));
+
+            $this->assertTrue($session->forget('foo'));
+        }
+
+        public function testHasChecksIfDataInSession()
+        {
+            $session = $this->makeSession();
+
+            $session->put('foo', 'bar');
+
+            $this->assertTrue($session->has('foo'));
+        }
+
+        public function testFlashStoresItemInSession()
+        {
+            $session = $this->makeSession();
+
+            $this->assertTrue($session->flash('foo', 'bar'));
+            $this->assertEquals('bar', $session->get('foo'));
+        }
+
+        protected function makeSession()
+        {
+            return new CodeigniterDriver();
+        }
+    }
+}
+
+namespace Michaeljennings\Carpenter\Session {
+    use Mockery as m;
+
+    function get_instance()
+    {
+        $object = new \stdClass();
+
+        $object->session = m::mock('Session', [
+            'userdata' => 'bar',
+            'set_userdata' => true,
+            'set_flashdata' => true,
+            'unset_userdata' => true,
+        ]);
+
+        return $object;
+    }
+}
