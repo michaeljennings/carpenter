b38cbfbb9e312f47ee7ac7e4599dab5a1bad0017
Michael Jennings <michael.jennings91@gmail.com>
2015-10-02 11:11:41 +0100

    Add label and href closure tests for action component

--- tests/components/ActionTest.php
+++ tests/components/ActionTest.php
@@ -66,6 +66,12 @@ class ActionTest extends TestCase
         $action = $table->action('create')->setLabel('Foo');
 
         $this->assertContains('Foo', $action->render());
+
+        $action->setLabel(function() {
+            return 'Test Value';
+        });
+
+        $this->assertContains('Test Value', $action->render());
     }
 
     public function testTagCanBeSet()
@@ -100,9 +106,61 @@ class ActionTest extends TestCase
     	$table = $this->makeTable();
 
         $action = $table->action('create')->setAttribute('ng-click', 'custom');
-        $action = $action->title('click this');
+        $action->title('click this');
+        $action->setAttribute('data-title', function() {
+            return 'TEST VALUE';
+        });
 
         $this->assertContains('ng-click="custom"', $action->render());
         $this->assertContains('title="click this"', $action->render());
+        $this->assertContains('data-title="TEST VALUE"', $action->render());
+    }
+
+    public function testActionColumnCanBeChangedFromDefault()
+    {
+        $table = $this->makeTableWithData();
+        $action = $table->action('edit', 'row');
+
+        $this->assertEquals('id', $action->getColumn());
+
+        $action->setColumn('foo');
+
+        $this->assertEquals('foo', $action->getColumn());
+    }
+
+    public function testValueAndRowArePassedToActionClosures()
+    {
+        $table = $this->makeTableWithData();
+        $action = $table->action('edit', 'row')->setColumn('foo')->setValue('Test')->setRow($this->getData()[0]);
+
+        $action->setLabel(function($value) {
+            return $value;
+        });
+
+        $this->assertContains('Test', $action->render());
+
+        $action->setLabel(function($value, $row) {
+            return $row['foo'];
+        });
+
+        $this->assertContains('Test 1', $action->render());
+    }
+
+    public function testClosureCanBePassedToHref()
+    {
+        $table = $this->makeTableWithData();
+        $action = $table->action('edit', 'row')->setColumn('foo')->setValue('Test')->setRow($this->getData()[0]);
+
+        $action->href(function($value) {
+            return 'edit/' . $value;
+        });
+
+        $this->assertContains('href="edit/Test"', $action->render());
+
+        $action->href(function($value, $row) {
+            return 'edit/' . $row['foo'];
+        });
+
+        $this->assertContains('href="edit/Test 1"', $action->render());
     }
 }
