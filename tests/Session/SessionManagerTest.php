86b804730c7fd41c7258e09ee15f97f00bc6cba2
Michael Jennings <michael.jennings91@gmail.com>
2015-10-07 13:42:16 +0100

    Update session manager test

--- tests/Session/SessionManagerTest.php
+++ tests/Session/SessionManagerTest.php
@@ -1,53 +1,93 @@
 <?php
 
-namespace Michaeljennings\Carpenter\Tests\Session;
+namespace Michaeljennings\Carpenter\Tests\Session {
 
-use Michaeljennings\Carpenter\Session\SessionManager;
-use Michaeljennings\Carpenter\Tests\TestCase;
+    use Michaeljennings\Carpenter\Session\SessionManager;
+    use Michaeljennings\Carpenter\Tests\TestCase;
 
-class SessionManagerTest extends TestCase
-{
-    public function testSessionManagerExtendsCoreManager()
+    class SessionManagerTest extends TestCase
     {
-        $manager = $this->makeSessionManager();
+        public function testSessionManagerExtendsCoreManager()
+        {
+            $manager = $this->makeSessionManager();
 
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Nexus\Manager', $manager);
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Nexus\Manager', $manager);
+        }
+
+        public function testGetDefaultReturnsDefaultDriver()
+        {
+            $manager = $this->makeSessionManager();
+
+            $this->assertInternalType('string', $manager->getDefaultDriver());
+            $this->assertEquals('native', $manager->getDefaultDriver());
+        }
+
+        public function testCallingMethodsOnManagerReturnsDefaultDriver()
+        {
+            $manager = $this->makeSessionManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Session\NativeDriver', $manager->driver());
+        }
+
+        public function testCodeigniterDriverCanBeReturned()
+        {
+            $manager = $this->makeSessionManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Session\CodeigniterDriver',
+                $manager->driver('codeigniter'));
+        }
+
+        public function testNativeDriverCanBeReturned()
+        {
+            $manager = $this->makeSessionManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Session\NativeDriver', $manager->driver('native'));
+        }
+
+        public function testIlluminateDriverCanBeReturned()
+        {
+            $manager = $this->makeSessionManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Session\IlluminateDriver', $manager->driver('illuminate'));
+        }
+
+        /**
+         * @expectedException \Michaeljennings\Carpenter\Exceptions\DriverNotFoundException
+         */
+        public function testErrorIsThrownIfDriverDoesNotExist()
+        {
+            $manager = $this->makeSessionManager();
+
+            $manager->driver('foo');
+        }
+
+        protected function makeSessionManager()
+        {
+            return new SessionManager($this->getConfig());
+        }
+    }
+}
+
+namespace Michaeljennings\Carpenter\Session {
+
+    use Illuminate\Session\Store;
+    use Symfony\Component\HttpFoundation\Session\Storage\Handler\NullSessionHandler;
+
+    function app()
+    {
+        $manager = new IlluminateManager();
+
+        return [
+            'session' => $manager,
+        ];
     }
 
-    public function testGetDefaultReturnsDefaultDriver()
+    class IlluminateManager
     {
-        $manager = $this->makeSessionManager();
-
-        $this->assertInternalType('string', $manager->getDefaultDriver());
-        $this->assertEquals('native', $manager->getDefaultDriver());
+        public function driver()
+        {
+            return new Store('coreplex.session', new NullSessionHandler());
+        }
     }
 
-    public function testCallingMethodsOnManagerReturnsDefaultDriver()
-    {
-        $manager = $this->makeSessionManager();
-
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Session\NativeDriver', $manager->driver());
-    }
-
-    public function testDriverCanBeSpecified()
-    {
-        $manager = $this->makeSessionManager();
-
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Session\NativeDriver', $manager->driver('native'));
-    }
-
-    /**
-     * @expectedException \Michaeljennings\Carpenter\Exceptions\DriverNotFoundException
-     */
-    public function testErrorIsThrownIfDriverDoesNotExist()
-    {
-        $manager = $this->makeSessionManager();
-
-        $manager->driver('foo');
-    }
-
-    protected function makeSessionManager()
-    {
-        return new SessionManager($this->getConfig());
-    }
 }
