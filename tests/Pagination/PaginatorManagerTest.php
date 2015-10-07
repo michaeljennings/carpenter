b5839c3557e8953d51375de7d1b710a85df24695
Michael Jennings <michael.jennings91@gmail.com>
2015-10-07 14:11:15 +0100

    Update paginator manager test

--- tests/Pagination/PaginatorManagerTest.php
+++ tests/Pagination/PaginatorManagerTest.php
@@ -1,53 +1,77 @@
 <?php
 
-namespace Michaeljennings\Carpenter\Tests\Pagination;
+namespace Michaeljennings\Carpenter\Tests\Pagination {
 
-use Michaeljennings\Carpenter\Pagination\PaginationManager;
-use Michaeljennings\Carpenter\Tests\TestCase;
+    use Michaeljennings\Carpenter\Pagination\PaginationManager;
+    use Michaeljennings\Carpenter\Tests\TestCase;
 
-class PaginatorManagerTest extends TestCase
-{
-    public function testManagerExtendsCoreManager()
+    class PaginatorManagerTest extends TestCase
     {
-        $manager = $this->makePaginatorManager();
+        public function testManagerExtendsCoreManager()
+        {
+            $manager = $this->makePaginatorManager();
 
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Nexus\Manager', $manager);
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Nexus\Manager', $manager);
+        }
+
+        public function testGetDefaultReturnsDefaultDriver()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $this->assertInternalType('string', $manager->getDefaultDriver());
+            $this->assertEquals('native', $manager->getDefaultDriver());
+        }
+
+        public function testCallingMethodsOnManagerReturnsDefaultDriver()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\Native', $manager->driver());
+        }
+
+        public function testNativeDriverCanBeReturned()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\Native', $manager->driver('native'));
+        }
+
+        public function testIlluminateDriverCanBeReturned()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\IlluminateDriver', $manager->driver('illuminate'));
+        }
+
+        public function testLaravel4DriverCanBeReturned()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\Laravel4\IlluminateDriver', $manager->driver('laravel4'));
+        }
+
+        /**
+         * @expectedException \Michaeljennings\Carpenter\Exceptions\DriverNotFoundException
+         */
+        public function testErrorIsThrownIfDriverDoesNotExist()
+        {
+            $manager = $this->makePaginatorManager();
+
+            $manager->driver('foo');
+        }
+
+        protected function makePaginatorManager()
+        {
+            return new PaginationManager($this->getConfig());
+        }
+    }
+}
+
+namespace Michaeljennings\Carpenter\Pagination {
+
+    function app()
+    {
+        return null;
     }
 
-    public function testGetDefaultReturnsDefaultDriver()
-    {
-        $manager = $this->makePaginatorManager();
-
-        $this->assertInternalType('string', $manager->getDefaultDriver());
-        $this->assertEquals('native', $manager->getDefaultDriver());
-    }
-
-    public function testCallingMethodsOnManagerReturnsDefaultDriver()
-    {
-        $manager = $this->makePaginatorManager();
-
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\Native', $manager->driver());
-    }
-
-    public function testDriverCanBeSpecified()
-    {
-        $manager = $this->makePaginatorManager();
-
-        $this->assertInstanceOf('Michaeljennings\Carpenter\Pagination\Native', $manager->driver('native'));
-    }
-
-    /**
-     * @expectedException \Michaeljennings\Carpenter\Exceptions\DriverNotFoundException
-     */
-    public function testErrorIsThrownIfDriverDoesNotExist()
-    {
-        $manager = $this->makePaginatorManager();
-
-        $manager->driver('foo');
-    }
-
-    protected function makePaginatorManager()
-    {
-        return new PaginationManager($this->getConfig());
-    }
 }
