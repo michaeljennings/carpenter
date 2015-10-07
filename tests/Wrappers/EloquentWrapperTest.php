07564b37efdb9806ed59ecfa62fdb5d6191d4b4b
Michael Jennings <michael.jennings91@gmail.com>
2015-10-07 13:56:48 +0100

    Add eloquent wrapper test

---
+++ tests/Wrappers/EloquentWrapperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests\Wrappers;
+
+use Michaeljennings\Carpenter\Tests\TestCase;
+use Michaeljennings\Carpenter\Wrappers\Eloquent;
+use Mockery as m;
+
+class EloquentWrapperTest extends TestCase
+{
+    public function testValueCanBeRetrieved()
+    {
+        $wrapper = $this->makeWrapper();
+
+        $this->assertEquals('bar', $wrapper->foo);
+    }
+
+    public function makeWrapper()
+    {
+        $model = m::mock('Illuminate\Database\Eloquent\Model', []);
+
+        $model->foo = 'bar';
+
+        return new Eloquent($model);
+    }
+}
