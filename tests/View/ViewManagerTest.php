c4a25198c435ba3300bf126f1b9cf9b48e214462
Michael Jennings <michael.jennings91@gmail.com>
2015-10-07 13:51:13 +0100

    Add view manager test

---
+++ tests/View/ViewManagerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests\View {
+
+    use Michaeljennings\Carpenter\Tests\TestCase;
+    use Michaeljennings\Carpenter\View\ViewManager;
+
+    class ViewManagerTest extends TestCase
+    {
+        public function testNativeDriverCanBeReturned()
+        {
+            $manager = $this->makeManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\View\Native', $manager->driver('native'));
+        }
+
+        public function testCodeigniterDriverCanBeReturned()
+        {
+            $manager = $this->makeManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\View\CodeigniterDriver',
+                $manager->driver('codeigniter'));
+        }
+
+        public function testIlluminateDriverCanBeReturned()
+        {
+            $manager = $this->makeManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\View\IlluminateDriver', $manager->driver('illuminate'));
+        }
+
+        public function testDefaultDriverIsReturnedIfNoneIsSpecified()
+        {
+            $manager = $this->makeManager();
+
+            $this->assertInstanceOf('Michaeljennings\Carpenter\View\Native', $manager->driver());
+        }
+
+        protected function makeManager()
+        {
+            return new ViewManager($this->getConfig());
+        }
+    }
+}
+
+namespace Michaeljennings\Carpenter\View {
+
+    use Illuminate\Container\Container;
+    use Illuminate\Events\Dispatcher;
+    use Illuminate\Filesystem\Filesystem;
+    use Illuminate\View\Engines\EngineResolver;
+    use Illuminate\View\Engines\PhpEngine;
+    use Illuminate\View\Factory;
+    use Illuminate\View\FileViewFinder;
+
+    function app()
+    {
+        $app = new Container();
+
+        $resolver = new EngineResolver;
+        $resolver->register('php', function () { return new PhpEngine; });
+
+        $finder = new FileViewFinder(new Filesystem, [realpath(__DIR__)]);
+
+        $dispatcher = (new Dispatcher($app))->setQueueResolver(function () use ($app) {
+            return $app->make('Illuminate\Contracts\Queue\Factory');
+        });
+
+        $env = new Factory($resolver, $finder, $dispatcher);
+
+        $env->setContainer($app);
+        $env->share('app', $app);
+
+        return [
+            'view' => $env
+        ];
+    }
+
+}
