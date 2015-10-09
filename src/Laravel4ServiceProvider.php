34b277966740f436cc91d6021974308f8af0f164
Michael Jennings <michael.jennings91@gmail.com>
2015-10-09 11:18:11 +0100

    Remove boot method in laravel 4 service provider from code coverage

--- src/Laravel4ServiceProvider.php
+++ src/Laravel4ServiceProvider.php
@@ -8,6 +8,7 @@ class Laravel4ServiceProvider extends CarpenterServiceProvider
      * Bootstrap the application events.
      *
      * @return void
+     * @codeCoverageIgnore
      */
     public function boot()
     {
