bf122c377c90fa00e468b64327d27243daaae131
Michael Jennings <michael.jennings91@gmail.com>
2015-10-09 10:48:55 +0100

    Add laravel 4 paginator test

---
+++ tests/Pagination/Laravel4PaginatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests\Pagination;
+
+use Michaeljennings\Carpenter\Pagination\Laravel4\IlluminateDriver;
+use Michaeljennings\Carpenter\Tests\TestCase;
+use Mockery as m;
+
+class Laravel4PaginatorTest extends TestCase
+{
+    public function testPaginatorImplementsContract()
+    {
+        $paginator = $this->makePaginator();
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Paginator', $paginator);
+    }
+
+    public function testMakeMethodReturnsInstance()
+    {
+        $paginator = $this->makePaginator();
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Paginator', $paginator->make(10, 5));
+    }
+
+    public function testLinksMethodReturnsString()
+    {
+        $paginator = $this->makePaginator();
+        $paginator->make(10, 5);
+
+        $this->assertInternalType('string', $paginator->links());
+    }
+
+    public function testCurrentPageMethodReturnsInt()
+    {
+        $paginator = $this->makePaginator();
+        $paginator->make(10, 5);
+
+        $this->assertInternalType('integer', $paginator->currentPage());
+    }
+
+    public function makePaginator()
+    {
+        $app = [
+            'paginator' => m::mock('Paginator', [
+                'make' => m::mock('Paginator', [
+                    'links' => '',
+                    'getCurrentPage' => 1
+                ]),
+            ]),
+        ];
+
+        return new IlluminateDriver($app);
+    }
+}
