04bc27941f74c629d5d94593d3f4a9e84408bc60
Michael Jennings <michael.jennings91@gmail.com>
2015-12-01 22:25:14 +0000

    Fix bug with count method in laravel 4 eloquent class

--- src/Store/Laravel4/Eloquent.php
+++ src/Store/Laravel4/Eloquent.php
@@ -3,17 +3,25 @@
 namespace Michaeljennings\Carpenter\Store\Laravel4;
 
 use Michaeljennings\Carpenter\Contracts\Store;
+use Michaeljennings\Carpenter\Exceptions\ModelNotSetException;
 use Michaeljennings\Carpenter\Store\Eloquent as Laravel5Store;
 
 class Eloquent extends Laravel5Store implements Store
 {
     /**
-     * Return the total results.
+     * Return the total results of the query.
      *
      * @return integer
+     * @throws ModelNotSetException
      */
     public function count()
     {
-        return $this->model->paginate(1, $this->select)->getTotal();
+        if ( ! $this->model) {
+            throw new ModelNotSetException('You must set a model to be used by the eloquent driver.');
+        }
+
+        $model = clone $this->model;
+
+        return $model->paginate(1, $this->select)->getTotal();
     }
 }
