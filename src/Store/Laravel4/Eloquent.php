a452cef693f7e8342ba773e149ee855661d274e8
Michael Jennings <michael.jennings91@gmail.com>
2016-01-26 13:43:54 +0000

    Fix paginate method in laravel 4 service provider (Fixes #9)

--- src/Store/Laravel4/Eloquent.php
+++ src/Store/Laravel4/Eloquent.php
@@ -9,6 +9,18 @@ use Michaeljennings\Carpenter\Store\Eloquent as Laravel5Store;
 class Eloquent extends Laravel5Store implements Store
 {
     /**
+     * An instance of the IOC container.
+     *
+     * @var mixed
+     */
+    protected $app;
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
      * Return the total results of the query.
      *
      * @return integer
@@ -23,5 +35,25 @@ class Eloquent extends Laravel5Store implements Store
         $model = clone $this->model;
 
         return $model->paginate(1, $this->select)->getTotal();
+    }
+
+    /**
+     * Return a paginated list of results.
+     *
+     * @param int|string $amount
+     * @param int|string $page
+     * @param int|string $perPage
+     * @return array
+     * @throws ModelNotSetException
+     */
+    public function paginate($amount, $page, $perPage)
+    {
+        if ( ! $this->model) {
+            throw new ModelNotSetException('You must set a model to be used by the eloquent driver.');
+        }
+
+        $this->app['paginator']->setPageName($this->key);
+
+        return $this->model->paginate($amount, $this->select)->all();
     }
 }
