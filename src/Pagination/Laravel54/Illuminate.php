385d8fcd801f2c82eb6e49069e6e91387f59a53b
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-03-10 16:09:02 +0000

    Added laravel 54 as separate driver

---
+++ src/Pagination/Laravel54/Illuminate.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Pagination;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Michaeljennings\Carpenter\Contracts\Paginator as PaginatorContract;
+
+class Laravel54 extends AbstractPaginator implements PaginatorContract
+{
+    /**
+     * An instance of the IOC container.
+     *
+     * @var mixed
+     */
+    protected $app;
+
+    /**
+     * The illuminate paginator.
+     *
+     * @var mixed
+     */
+    protected $paginator;
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Create a new paginator.
+     *
+     * @param  string|integer $total
+     * @param  string|integer $perPage
+     * @param  string         $tableKey
+     * @return $this
+     */
+    public function make($total, $perPage, $tableKey)
+    {
+        $this->paginator = new LengthAwarePaginator([], $total, $perPage, $this->app['request']->input($tableKey), [
+            'path' => $this->app['request']->url(),
+        ]);
+
+        $this->paginator->setPageName($tableKey);
+
+        return $this;
+    }
+
+    /**
+     * Get the pagination links.
+     *
+     * @return string
+     */
+    public function links()
+    {
+        return $this->paginator->render();
+    }
+
+    /**
+     * Get the current page.
+     *
+     * @return integer|string
+     */
+    public function currentPage()
+    {
+        return $this->paginator->currentPage();
+    }
+} 
---
+++ tests/Pagination/Laravel54PaginatorTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Michaeljennings\Carpenter\Tests\Pagination;
+
+use Michaeljennings\Carpenter\Pagination\Illuminate;
+use Michaeljennings\Carpenter\Tests\TestCase;
+use Mockery as m;
+
+class IlluminatePaginatorTest extends TestCase
+{
+    protected $page = 1;
+
+    public function testPaginatorImplementsContract()
+    {
+        $paginator = $this->makePaginator();
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Paginator', $paginator);
+    }
+
+    public function testMakeReturnsPaginator()
+    {
+        $paginator = $this->makePaginator();
+
+        $this->assertInstanceOf('Michaeljennings\Carpenter\Contracts\Paginator', $paginator->make(1, 10, 'page'));
+    }
+
+    public function testLinksReturnsEmptyIfThereAreNoLinks()
+    {
+        $paginator = $this->makePaginator();
+        $paginator->make(1, 10, 'page');
+
+        $this->assertEmpty($paginator->links());
+    }
+
+    public function testLinksAreRenderedToAList()
+    {
+        $paginator = $this->makePaginator();
+        $paginator->make(30, 10, 'page');
+
+        $links = $paginator->links();
+
+        $this->assertContains('ul', $links);
+        $this->assertContains('li', $links);
+        $this->assertContains('1', $links);
+    }
+
+    public function testPrevIsShownIfNotOnFirstPage()
+    {
+        $this->setPage(2);
+
+        $paginator = $this->makePaginator();
+        $paginator->make(30, 10, 'page');
+
+        $links = $paginator->links();
+        $this->assertContains('prev', $links);
+        $this->setPage(1);
+    }
+
+    public function testNextIsNotShownIfOnLastPage()
+    {
+        $this->setPage(3);
+
+        $paginator = $this->makePaginator();
+        $paginator->make(30, 10, 'page');
+
+        $links = $paginator->links();
+        $this->assertNotContains('next', $links);
+        $this->setPage(1);
+    }
+
+    public function testCurrentPageReturnsTheCorrectPage()
+    {
+        $paginator = $this->makePaginator();
+        $paginator->make(30, 10, 'page');
+
+        $this->assertEquals(1, $paginator->currentPage());
+    }
+    protected function makePaginator()
+    {
+        $app = [
+            'request' => m::mock('Illuminate\Http\Request', [
+                'input' => $this->page,
+                'url' => 'http://localhost',
+            ])
+        ];
+
+        return new Illuminate($app);
+    }
+
+    protected function setPage($page = 1)
+    {
+        $this->page = $page;
+    }
+}
